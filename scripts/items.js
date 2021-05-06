5e7d1fdf92fe527a67629acb91ab961a7036bc8f
Tom Clouse <tommyclouse@yahoo.com>
2021-05-06 16:07:09 -0500

    adding the item horizontal radio buttons

---
+++ scripts/items.js
@@ -0,0 +1,35 @@
+import { getItems, setItem } from "./database.js"
+
+const items = getItems()
+
+document.addEventListener(
+    "change",
+    (event) => {
+        if (event.target.name === "item") {
+            const[,itemId] = event.target.value.split("--")
+
+            setItem(parseInt(itemId))
+
+        }
+    }
+)
+
+
+
+
+export const JewelryItems = () => {
+    let html = "<ul class='item-options'>"
+
+    const itemOptions = items.map((jewelryItem) => 
+    {
+        return `
+        <li class = "list-item">
+        <input type="radio" name="item" value ="item--${jewelryItem.id}">${jewelryItem.item}
+        </li>
+        `
+    })
+
+    html += itemOptions.join("")
+    html += "</ul>"
+    return html
+}
